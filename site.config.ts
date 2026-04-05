73d4b83ebb2040b96d0349033f1fda3567686b9b
Fox <49583491+FoxSpain@users.noreply.github.com>
2026-04-05 08:56:41 +0800

    Disable social image support in site configuration
    Disable social image support to resolve 1MB limit error.

--- site.config.ts
+++ site.config.ts
@@ -38,6 +38,9 @@ export default siteConfig({
   // environment variables. see the readme for more info
   isRedisEnabled: false,
 
+  // 【插入这一行】关闭社交预览图生成，解决 1MB 限制报错
+  isSocialImageSupportEnabled: false,
+  
   // map of notion page IDs to URL paths (optional)
   // any pages defined here will override their default URL paths
   // example:
