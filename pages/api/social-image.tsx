a3591d62c56ef26a909b4c297e4d6ccba5f21add
Fox <49583491+FoxSpain@users.noreply.github.com>
2026-04-05 09:05:44 +0800

    Delete pages/api/social-image.tsx

--- pages/api/social-image.tsx
+++
@@ -1,7 +0,0 @@
-export const runtime = 'edge'
-
-export default async function OGImage() {
-  return new Response('Social image generation is disabled to save build size.', {
-    status: 200
-  })
-}
